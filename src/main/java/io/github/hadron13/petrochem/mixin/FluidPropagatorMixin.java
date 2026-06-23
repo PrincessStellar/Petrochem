89958f05a13ab77a2a1345f2545ad8c803d1826a
hadron13 <carlosbpfonseca@gmail.com>
2026-06-23 08:43:31 -0300

    Updates people, updates

--- src/main/java/io/github/hadron13/petrochem/mixin/FluidPropagatorMixin.java
+++ src/main/java/io/github/hadron13/petrochem/mixin/FluidPropagatorMixin.java
@@ -10,32 +10,32 @@ import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
-@Mixin(FluidPropagator.class)
+//@Mixin(FluidPropagator.class)
 public class FluidPropagatorMixin {
 
     //kept as a memorial to my first Create PR :PP
     //https://github.com/Creators-of-Create/Create/pull/9763
-
-    @WrapOperation(method = "propagateChangedPipe",
-        remap = false,
-        at = @At(
-            value = "INVOKE",
-            target = "Lcom/tterrag/registrate/util/entry/BlockEntry;has(Lnet/minecraft/world/level/block/state/BlockState;)Z"
-        )
-    )
-    private static boolean pumpWrapper(BlockEntry instance, BlockState state, Operation<Boolean> original){
-        return original.call(instance, state) || state.getBlock() instanceof PumpBlock;
-    }
-
-
-    @WrapOperation(method = "validateNeighbourChange",
-        remap = false,
-        at = @At(
-            value = "INVOKE",
-            target = "Lcom/tterrag/registrate/util/entry/BlockEntry;has(Lnet/minecraft/world/level/block/state/BlockState;)Z"
-        )
-    )
-    private static boolean encasedPipeWrapper(BlockEntry instance, BlockState state, Operation<Boolean> original){
-        return original.call(instance, state) || state.getBlock() instanceof EncasedPipeBlock;
-    }
+//
+//    @WrapOperation(method = "propagateChangedPipe",
+//        remap = false,
+//        at = @At(
+//            value = "INVOKE",
+//            target = "Lcom/tterrag/registrate/util/entry/BlockEntry;has(Lnet/minecraft/world/level/block/state/BlockState;)Z"
+//        )
+//    )
+//    private static boolean pumpWrapper(BlockEntry instance, BlockState state, Operation<Boolean> original){
+//        return original.call(instance, state) || state.getBlock() instanceof PumpBlock;
+//    }
+//
+//
+//    @WrapOperation(method = "validateNeighbourChange",
+//        remap = false,
+//        at = @At(
+//            value = "INVOKE",
+//            target = "Lcom/tterrag/registrate/util/entry/BlockEntry;has(Lnet/minecraft/world/level/block/state/BlockState;)Z"
+//        )
+//    )
+//    private static boolean encasedPipeWrapper(BlockEntry instance, BlockState state, Operation<Boolean> original){
+//        return original.call(instance, state) || state.getBlock() instanceof EncasedPipeBlock;
+//    }
 }
